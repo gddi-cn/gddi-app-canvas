a4c1ef72bcfbeb511010f94a762b9e6bcbf0c9db
daisy.pang <pangruoyun@gddi.com.cn>
2021-12-29 19:19:14 +0800

    chore: tsconfig excludes stories

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser'
 
 const packageJson = require('./package.json')
 
@@ -26,6 +27,7 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    postcss()
+    postcss(),
+    terser()
   ]
 }
