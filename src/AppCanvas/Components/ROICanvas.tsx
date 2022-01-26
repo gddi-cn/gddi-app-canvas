371efa05bdc592e811d03adc10278c036f12bf24
daisy.pang <daisyrpanglive@live.com>
2022-01-26 18:05:47 +0800

    feat: canvas add panning

--- src/AppCanvas/Components/ROICanvas.tsx
+++ src/AppCanvas/Components/ROICanvas.tsx
@@ -29,6 +29,9 @@ export function ROICanvas({
   const appRef = useRef<fabric.Canvas | undefined>(undefined)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const imgRef = useRef<fabric.Image | undefined>(undefined)
+  const isDragging = useRef<boolean>(false)
+  const lastPosX = useRef<number>(0)
+  const lastPosY = useRef<number>(0)
 
   const handleCanvasWheel = useCallback(
     (opt: fabric.IEvent): void => {
@@ -46,6 +49,78 @@ export function ROICanvas({
     },
     [appRef.current]
   )
+
+  const handleCanvasDown = useCallback((opt: fabric.IEvent): void => {
+    startPan(opt)
+    // if (toolType === 'drawBox' && this.canvasInstance && !panning) {
+    //   const pt = this.canvasInstance.getPointer(opt.e as any)
+    //   if (this.drawRectAssistant.Status === 'init') {
+    //     this.drawRectAssistant.setPoint(pt.x, pt.y)
+    //     this.drawRectAssistant.addToCanvas(this.canvasInstance)
+    //     this.canvasInstance.requestRenderAll()
+    //   } else if (this.drawRectAssistant.Status === 'pt1') {
+    //     // end draw -- create new box
+    //     this.drawRectAssistant.setPoint(pt.x, pt.y)
+    //     const { top, left, width, height } = this.drawRectAssistant.endDraw()
+    //     this.addNewBox(
+    //       Math.round(top),
+    //       Math.round(left),
+    //       Math.round(width),
+    //       Math.round(height)
+    //     )
+    //     this.drawRectAssistant.removeFromCanvas()
+    //   }
+    // }
+  }, [])
+
+  const handleCanvasMove = useCallback((opt: fabric.IEvent): void => {
+    panMoving(opt)
+    // if (this.canvasInstance) {
+    //   const evt = opt.e as any
+    //   const pt = this.canvasInstance.getPointer(evt)
+    //   onMouseMove(evt.offsetX, evt.offsetY, pt.x, pt.y)
+    //   if (toolType === 'drawBox' && this.drawRectAssistant.Status === 'pt1') {
+    //     this.drawRectAssistant.setPoint(pt.x, pt.y)
+    //     this.canvasInstance.requestRenderAll()
+    //   }
+    // }
+  }, [])
+
+  const handleCanvasUp = useCallback((): void => {
+    endPan()
+  }, [])
+
+  const startPan = useCallback((opt: fabric.IEvent): void => {
+    const evt = opt.e as any
+    if (appRef.current) {
+      isDragging.current = true
+      lastPosX.current = evt.clientX
+      lastPosY.current = evt.clientY
+    }
+  }, [])
+
+  const panMoving = useCallback((opt: fabric.IEvent): void => {
+    if (isDragging.current && appRef.current) {
+      const e = opt.e as any
+      const vpt = appRef.current.viewportTransform
+      if (vpt) {
+        vpt[4] += e.clientX - lastPosX.current
+        vpt[5] += e.clientY - lastPosY.current
+        appRef.current.requestRenderAll()
+      }
+      lastPosX.current = e.clientX
+      lastPosY.current = e.clientY
+    }
+  }, [])
+
+  const endPan = useCallback((): void => {
+    // on mouse up we want to recalculate new interaction
+    // for all objects, so we call setViewportTransform
+    if (appRef.current && appRef.current.viewportTransform) {
+      appRef.current.setViewportTransform(appRef.current.viewportTransform)
+      isDragging.current = false
+    }
+  }, [])
 
   useEffect(() => {
     // init image
@@ -80,10 +155,9 @@ export function ROICanvas({
         uniformScaling: false
       })
       app.on('mouse:wheel', handleCanvasWheel)
-      //   app.on('mouse:down', handleCanvasDown)
-      //   app.on('mouse:move', handleCanvasMove)
-      //   app.on('mouse:up', handleCanvasUp)
-      //   app.on('mouse:over', handleCanvasOver)
+      app.on('mouse:down', handleCanvasDown)
+      app.on('mouse:move', handleCanvasMove)
+      app.on('mouse:up', handleCanvasUp)
 
       appRef.current = app
       if (imgUrl) {
