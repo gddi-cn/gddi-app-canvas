ac348d04e233c39ba321afa4b8be1887cdeb1310
daisy.pang <pangruoyun@gddi.com.cn>
2022-04-20 18:36:08 +0800

    fix: wrong optional field in io-ts

--- src/AppCanvas/types/modelDefinitions.ts
+++ src/AppCanvas/types/modelDefinitions.ts
@@ -1,43 +1,64 @@
-import * as t from 'io-ts';
+import * as t from 'io-ts'
 
 export const tEndpoint = t.type({
-    id: t.number,
-    name: t.string
+  id: t.number,
+  name: t.string
 })
 
-export const tBasicTypeName = t.union([t.literal('string'), t.literal('boolean'), t.literal('number')])
+export const tBasicTypeName = t.union([
+  t.literal('string'),
+  t.literal('boolean'),
+  t.literal('number')
+])
 export const tBasicType = t.union([t.string, t.boolean, t.number])
 
-export const tSimplePropDefinition = t.type({
-    type: t.union([tBasicTypeName, t.literal('stringArray'), t.literal('array')]),
-    description: t.union([t.undefined, t.string]),
-    default: t.union([tBasicType, t.undefined]),
+export const tPropDefBasic = t.type({
+  type: t.union([tBasicTypeName, t.literal('stringArray'), t.literal('array')])
+})
+
+export const tSimplePropDefinition = t.intersection([
+  tPropDefBasic,
+  t.partial({
+    description: t.string,
+    default: tBasicType,
     // [case] type is number or integer
-    maximum: t.union([t.undefined, t.number]),
-    minimum: t.union([t.undefined, t.number]),
+    maximum: t.number,
+    minimum: t.number,
     // [case] type is BasicType; single select
-    enum: t.union([t.undefined, t.array(tBasicType)])
-})
+    enum: t.array(tBasicType)
+  })
+])
 
-export const tROIPropDefinition = t.type({
-    type: t.union([tBasicTypeName, t.literal('stringArray'), t.literal('array')]),
-    description: t.union([t.undefined, t.string]),
-    minDimension: t.union([t.undefined, t.number]),
-    maxDimension: t.union([t.undefined, t.number]),
-})
+export const tROIPropDefinition = t.intersection([
+  tPropDefBasic,
+  t.partial({
+    description: t.string,
+    minDimension: t.number,
+    maxDimension: t.number
+  })
+])
 
-export const tPropDefinitionType = t.intersection([tSimplePropDefinition, tROIPropDefinition])
+export const tPropDefinitionType = t.intersection([
+  tSimplePropDefinition,
+  tROIPropDefinition
+])
+
+export type PropDefinitionType = t.TypeOf<typeof tPropDefinitionType>
 
 export const tPropDefinitionObject = t.record(t.string, tPropDefinitionType)
 
-export const tModuleDefinition = t.type({
+export const tModuleDefinition = t.intersection([
+  t.type({
     version: t.string,
     name: t.string,
-    description: t.string,
-    inputs: t.union([t.array(tEndpoint), t.undefined]),
-    outputs: t.union([t.array(tEndpoint), t.undefined]),
-    props: t.union([tPropDefinitionObject, t.undefined])
-})
+    description: t.string
+  }),
+  t.partial({
+    inputs: t.array(tEndpoint),
+    outputs: t.array(tEndpoint),
+    props: tPropDefinitionObject
+  })
+])
 export type ModuleDefinition = t.TypeOf<typeof tModuleDefinition>
 
 export const tModuleDefinitions = t.record(t.string, tModuleDefinition)
